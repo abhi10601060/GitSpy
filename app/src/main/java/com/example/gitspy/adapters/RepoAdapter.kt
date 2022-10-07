21150bcf27f9d05c4151fd7173cdf18b32276d7c
Abhi <abhishekvelekar79200@gmail.com>
2022-10-07 22:23:09 +0530

    adapter added for repositories

---
+++ app/src/main/java/com/example/gitspy/adapters/RepoAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.gitspy.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.gitspy.R
+import com.example.gitspy.models.Item
+
+
+class RepoAdapter(private val context: Context) : ListAdapter<Item, RepoAdapter.ViewHolder>(DiffutilCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.repository_layout , parent , false)
+        return  ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val repo = getItem(position)
+
+        Glide.with(context)
+            .load(repo.owner.avatar_url)
+            .into(holder.logo)
+
+        holder.repoName.text = repo.name
+        holder.owner.text = repo.owner.login
+        holder.desc.text = repo.description
+        holder.stars.text = "${repo.stargazers_count} stars"
+        repo.language.let {
+            holder.language.text = repo.language
+        }
+        holder.forks.text = "${repo.forks_count} Forks"
+
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val logo : ImageView = itemView.findViewById(R.id.repoLogoImg)
+        val repoName : TextView = itemView.findViewById(R.id.repoNameText)
+        val owner : TextView = itemView.findViewById(R.id.ownerNameText)
+        val desc :TextView = itemView.findViewById(R.id.repoDescText)
+        val stars : TextView = itemView.findViewById(R.id.repoStarsText)
+        val language : TextView = itemView.findViewById(R.id.repoLanguageText)
+        val forks : TextView = itemView.findViewById(R.id.repoForksText)
+        val trackBtn : Button = itemView.findViewById(R.id.trackRepoBtn)
+
+    }
+
+    class DiffutilCallback : DiffUtil.ItemCallback<Item>() {
+        override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Item, newItem: Item): Boolean {
+            return  oldItem==newItem
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/gitspy/ui/fragments/RepositoryFragment.kt
+++ app/src/main/java/com/example/gitspy/ui/fragments/RepositoryFragment.kt
@@ -1,11 +1,14 @@
 package com.example.gitspy.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gitspy.R
+import com.example.gitspy.adapters.RepoAdapter
 import com.example.gitspy.models.RepoList
 import com.example.gitspy.ui.activities.MainActivity
 import com.example.gitspy.utility.Resource
@@ -15,19 +18,44 @@ import kotlinx.android.synthetic.main.fragment_repository.*
 class RepositoryFragment : Fragment(R.layout.fragment_repository) {
 
     private lateinit var viewModel : MainViewModel
+    private lateinit var adapter: RepoAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = (activity as MainActivity).viewModel
 
-        viewModel.getRepos("Karter")
+        repoSearchButton.setOnClickListener(View.OnClickListener {
+            val repoName = repoSearchBox.text.toString()
+            viewModel.getRepos(repoName)
+        })
 
+        adapter = RepoAdapter(requireContext())
         viewModel.repoList.observe(viewLifecycleOwner , Observer {
-            if (it is Resource.Success<RepoList>){
-                repotext.text = it.data?.items.toString()
+            when(it){
+
+                is Resource.Success<RepoList> -> {
+                    searchRepoProgressBar.visibility = View.GONE
+                    adapter.submitList(it.data?.items)
+                    searchRepoRV.adapter = adapter
+                    searchRepoRV.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+                }
+
+                is Resource.Loading<RepoList> -> {
+                   searchRepoProgressBar.visibility = View.VISIBLE
+                }
+
+                is Resource.Error<RepoList> -> {
+                    searchRepoProgressBar.visibility = View.GONE
+                    Log.d("ABHI", "onViewCreated: Error happened during fetching repos...")
+                }
+
             }
         })
+
+
+
+
 
     }
 }
