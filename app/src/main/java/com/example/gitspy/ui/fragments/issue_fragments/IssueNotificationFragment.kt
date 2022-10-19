9c5da79f2846606d95517a7e5b0b6ec2bccac38d
Abhi <abhishekvelekar79200@gmail.com>
2022-10-19 13:30:53 +0530

    issueNotification Fragment is added with recyclerview

--- app/src/main/java/com/example/gitspy/ui/fragments/issue_fragments/IssueNotificationFragment.kt
+++ app/src/main/java/com/example/gitspy/ui/fragments/issue_fragments/IssueNotificationFragment.kt
@@ -3,11 +3,28 @@ package com.example.gitspy.ui.fragments.issue_fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.gitspy.R
+import com.example.gitspy.adapters.IssueNotificationAdapter
+import com.example.gitspy.ui.activities.StatsActivity
+import com.example.gitspy.viewmodels.StatsViewModel
+import kotlinx.android.synthetic.main.fragment_issue_notifications.*
 
 class IssueNotificationFragment : Fragment(R.layout.fragment_issue_notifications) {
 
+    lateinit var  viewModel: StatsViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = (activity as StatsActivity).viewModel
+
+        viewModel.issue.observe(viewLifecycleOwner , Observer {
+            val adapter = IssueNotificationAdapter()
+            adapter.submitList(it.reversed())
+            issueNotificationRV.adapter = adapter
+            issueNotificationRV.layoutManager = LinearLayoutManager(requireContext() , LinearLayoutManager.VERTICAL , false)
+        })
     }
 }
