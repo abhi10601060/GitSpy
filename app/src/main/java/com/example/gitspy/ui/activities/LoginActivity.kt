5652c9f8478383659abc9ecedbd47151cab02b64
Abhi <abhishekvelekar79200@gmail.com>
2022-11-18 19:38:08 +0530

    Login Activity layout added

---
+++ app/src/main/java/com/example/gitspy/ui/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gitspy.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gitspy.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
