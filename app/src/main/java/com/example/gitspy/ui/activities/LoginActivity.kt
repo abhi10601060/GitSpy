1e16e59a1f0f7eea2a59fd2687cb32a4c02b1085
Abhi <abhishekvelekar79200@gmail.com>
2022-11-19 11:25:32 +0530

    Intent to authorize gitspy with github added

--- app/src/main/java/com/example/gitspy/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/gitspy/ui/activities/LoginActivity.kt
@@ -1,12 +1,34 @@
 package com.example.gitspy.ui.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.example.gitspy.R
 
+
+const val CLIENT_ID = "a6eebbf6dd9422605a92"
+const val CLIENT_SECRET = "b0b40372ad636f373efd2ee544700fca7dc06a6e"
+const val CALLBACK_URI = "gitspy://callback"
+
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var authenticate : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        authenticate = findViewById(R.id.login_btn_authenticate)
+
+        authenticate.setOnClickListener(View.OnClickListener {
+            var intent  = Intent(Intent.ACTION_VIEW , Uri.parse("http://github.com/login/oauth/authorize?client_id=$CLIENT_ID&scope=repo&redirect_uri=$CALLBACK_URI"))
+            startActivity(intent)
+        })
+
     }
+
 }
